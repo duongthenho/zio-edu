4416973ed6e4dbc598f5386b26e3774fb7f6f1b2
duongthenho <69722419+duongthenho@users.noreply.github.com>
2025-06-24 10:01:20 +0700

    refactor code

--- main.js
+++ main.js
@@ -15,3 +15,22 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+const toggleBtn = document.getElementById('toggleSidebar');
+const closeBtn = document.getElementById('closeSidebar');
+const sidebar = document.querySelector('nav.sidebar');
+const overlay = document.getElementById('overlaySidebar');
+
+toggleBtn?.addEventListener('click', () => {
+  sidebar?.classList.add('show');
+  overlay?.classList.add('show');
+});
+
+closeBtn?.addEventListener('click', () => {
+  sidebar?.classList.remove('show');
+  overlay?.classList.remove('show');
+});
+
+overlay?.addEventListener('click', () => {
+  sidebar?.classList.remove('show');
+  overlay?.classList.remove('show');
+});
